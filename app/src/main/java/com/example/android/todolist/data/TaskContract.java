370b394cfc24c96efcfa6e25c6d467543101d2f2
Sergey Kulyk <sergiokulyk98@gmail.com>
2018-05-09 09:19:30 +0300

    1.5 Add global vars for build content uri.

--- app/src/main/java/com/example/android/todolist/data/TaskContract.java
+++ app/src/main/java/com/example/android/todolist/data/TaskContract.java
@@ -16,15 +16,24 @@
 
 package com.example.android.todolist.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
+
+import java.net.URI;
 
 
 public class TaskContract {
 
+    public static final String AUTHORITY = "com.example.android";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    public static final String PATH = "todolist";
 
     /* TaskEntry is an inner class that defines the contents of the task table */
     public static final class TaskEntry implements BaseColumns {
-
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH).build();
 
         // Task table and column names
         public static final String TABLE_NAME = "tasks";
